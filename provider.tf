43ad75fe5c1bacbb0ddc3d47a6cab18685f37a25
aravindav <aravindwaitz@gmail.com>
2025-04-21 04:25:45 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region = var.aws_region
+
+  assume_role {
+    role_arn     = var.role_arn
+    session_name = "terraform-sts"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = var.s3_bucket
+    key            = var.s3_key
+    region         = var.aws_region
+    role_arn       = var.role_arn
+    dynamodb_table = var.dynamodb_table
+  }
 }
