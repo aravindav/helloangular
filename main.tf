74826a55137af2883789903a7d3bb43a2b28e940
aravindav <aravindwaitz@gmail.com>
2025-04-21 04:26:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,9 @@
-# EC2 Instance
-resource "aws_instance" "web" {
+resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
 
   tags = {
-    Name = "Terraform-EC2"
-  }
-}
-
-# S3 Bucket
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name = "Terraform-Bucket"
+    Name = "Terraform-Instance"
   }
 }
