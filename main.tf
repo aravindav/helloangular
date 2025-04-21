c9f451ae722ba6269de4ed7bbb92305a9e347070
aravindav <aravindwaitz@gmail.com>
2025-04-21 16:41:20 -0400

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "this" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = "Terraform-Instance"
-  }
-}
