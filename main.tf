017bcd2d9dfb54a1f1911081260b85a9d600fc7e
aravindav <aravindwaitz@gmail.com>
2025-04-21 16:43:01 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+# configured aws provider and backend with proper credentials
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::640111764884:role/stsassume-role"
+    session_name = "terraform-sts"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket         = "rs-terraform-statefile01"
+    key            = "terraform-statefile"
+    region         = "us-west-1"
+    role_arn       = "arn:aws:iam::640111764884:role/stsassume-role"
+    dynamodb_table = "rs-terraform-statetable"
+  }
+}
+# Define the AWS instance
+resource "aws_instance" "this" {
+  ami           = "ami-073e64e4c237c08ad" # us-east-1
+  instance_type = "t2.micro"
+  key_name      = "awskeypair"
+  tags = {
+    Name = "Terraform-Instance"
+  }
+}
